a9b675dd4f32db0c6775f249c126c2f5de4554db
Bojan Reljic <bojan@bitbank.lu>
2016-06-10 16:40:18 +0200

    implements petDelete controller on client

--- client/js/petsCtrl.js
+++ client/js/petsCtrl.js
@@ -47,3 +47,36 @@ elpets.controller('AddPetCtrl', ['$scope', '$http', function($scope, $http) {
     
 }]);
 
+elpets.controller('DeletePetCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+    $scope.pet = {};
+    $scope.pet.id = $routeParams.id;
+    $http.get('http://localhost:8080/pet/'+$scope.pet.id).
+        success(function(data, status, headers, config) {
+            $scope.pets = data[0];
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.pets = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+    $scope.deletePet = function() {
+        $http.delete('http://localhost:8080/pet/'+$scope.pet.id).
+        success(function(data, status, headers, config) {
+            $scope.DeleteResult = 'Success!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+        }).error(function (data, status, headers, config) {
+            $scope.DeleteResult = 'An Error Occured!';
+            $scope.data = data;
+            $scope.status = status;
+            $scope.headers = headers;
+            $scope.config = config;
+            });
+
+    }
+}]);
